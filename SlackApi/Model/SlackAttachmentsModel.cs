1af77f1d014b5efaea22461ece98eb93fb227e8e
veysel <vinanc72@gmail.com>
2018-10-30 22:42:24 +0300

    Model class added

---
+++ SlackApi/Model/SlackAttachmentsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackApi.Model
+{
+    public class SlackAttachmentsModel
+    {
+        public List<SlackModel> attachments { get; set; }
+
+        public SlackAttachmentsModel()
+        {
+            attachments = new List<SlackModel>();
+        }
+    }
+}
---
+++ SlackApi/Model/SlackModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackApi.Model
+{
+    public class SlackModel
+    {
+        public string color { get; set; }
+        public string author_name { get; set; }
+        public string text { get; set; }
+        public string footer { get; set; }
+    }
+}
